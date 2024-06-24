61ff5ffbc7857eedf95e823701c63ee9e4945a57
lynn demo <hxl1717@gmail.com>
2024-06-24 22:52:34 +0800

    Initial Commit

---
+++ Shared/helloswiftdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloswiftdemoApp.swift
+//  Shared
+//
+//  Created by Lynn on 2024/6/24.
+//
+
+import SwiftUI
+
+@main
+struct helloswiftdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
